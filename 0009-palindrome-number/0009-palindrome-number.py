00bd56c182fb30a00fda639948407141e260044c
Jeon Jihwan <127182495+jeonjihwan72@users.noreply.github.com>
2026-06-23 19:10:06 +0900

    Time: 8 ms (71.36%), Space: 12.4 MB (17.59%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,20 +4,31 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        # 숫자를 문자열로 변환
-        num_string = str(x)
-
-        start_index = 0
-        end_index = len(num_string)-1
         
-        is_palindrome = True
+        ### 포인터를 양쪽 끝에서부터 접근시키는 방법
+        # # 숫자를 문자열로 변환
+        # num_string = str(x)
 
-        while start_index < end_index:
-            if num_string[start_index] != num_string[end_index]:
-                is_palindrome = False
-                break
+        # start_index = 0
+        # end_index = len(num_string)-1
+        
+        # is_palindrome = True
+
+        # while start_index < end_index:
+        #     if num_string[start_index] != num_string[end_index]:
+        #         is_palindrome = False
+        #         break
             
-            start_index += 1
-            end_index -= 1
+        #     start_index += 1
+        #     end_index -= 1
         
-        return is_palindrome
+        # return is_palindrome
+
+        # 문자열 자체를 뒤집어 비교하는 방법
+        num_string = str(x)
+        reverse_x = num_string[::-1]
+
+        if num_string == reverse_x: return True
+        else: return False
+
+        
