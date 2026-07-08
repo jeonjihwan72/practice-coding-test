ceb224bbac255852ce0a0c778af0f6c9556c7911
Jeon Jihwan <127182495+jeonjihwan72@users.noreply.github.com>
2026-07-09 01:41:13 +0900

    Time: 2 ms (32.69%), Space: 12.5 MB (33.11%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        result = ""
+        checker = True
+        temp = sorted(strs, key=lambda x: len(x))
+
+        for i in range(0, len(temp[0])):
+            t = temp[0][i]
+            for w in strs:
+                if w[i] != t:
+                    checker = False
+                    break
+            
+            if checker == False:
+                break
+            
+            result += t
+        
+        return result
