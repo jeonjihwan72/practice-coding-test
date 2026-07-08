9a7a54c39fcc7024ed739e17ae7e94f77018bdba
Jeon Jihwan <127182495+jeonjihwan72@users.noreply.github.com>
2026-07-09 01:46:49 +0900

    Time: 0 ms (100%), Space: 12.5 MB (33.11%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -5,20 +5,33 @@ class Solution(object):
         :rtype: str
         """
 
-        result = ""
-        checker = True
-        temp = sorted(strs, key=lambda x: len(x))
+        # result = ""
+        # checker = True
+        # temp = sorted(strs, key=lambda x: len(x))
 
-        for i in range(0, len(temp[0])):
-            t = temp[0][i]
-            for w in strs:
-                if w[i] != t:
-                    checker = False
-                    break
+        # for i in range(0, len(temp[0])):
+        #     t = temp[0][i]
+        #     for w in strs:
+        #         if w[i] != t:
+        #             checker = False
+        #             break
             
-            if checker == False:
+        #     if checker == False:
+        #         break
+            
+        #     result += t
+        
+        # return result
+
+        result = ""
+        strs.sort()
+        
+        first = strs[0]
+        last = strs[-1]
+
+        for i in range(0, min(len(first), len(last))):
+            if first[i] != last[i]:
                 break
-            
-            result += t
+            result += first[i]
         
         return result
